9f6f4db27fc337fbf4eae8d82c5e2c1ad2e6c0d1
gregorymususa <g.mususa@hotmail.com>
2019-08-11 17:35:09 +0000

    In Progress: #67 Implement Privacy and Terms and Conditions for our business

--- urls.py
+++ urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('category/<slug:category_name_slug>/', views.category, name="category"),
     path('privacy/', views.privacy, name="privacy"),
     path('terms/', views.terms, name="terms"),
+    path('legal/', views.legal, name="legal"),
     path('results/', views.search, name="results"),
     path('ajax_search/', views.ajax_search, name="ajax_search"),
     path('ajax_category/<slug:category_name_slug>/', views.ajax_category, name="ajax_category"),
